925c1326e7f9273cdfb7c4f9a5172e0706f0b97f
Jeremiah Hammond <jeremiahhammond14@gmail.com>
2024-04-27 18:30:50 -0600

    update]

--- weathereye/execute_playbooks.py
+++ weathereye/execute_playbooks.py
@@ -3,7 +3,6 @@ import logging
 import click
 import ansible_runner
 import getpass
-import subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -47,17 +46,6 @@ def remote_run_docker_playbook():
     logging.warning("Attempting to execute docker playbook...")
 
     try:
-        # inventory path
-        inventory_path = os.path.dirname(__file__) + "/playbooks/inventory"
-
-        # custom ansible config path
-        custom_config_path = os.path.dirname(__file__) + "/playbooks/ansible_custom.cfg"
-
-        # appending inventory path to custom ansible config file
-        custom_config = open(custom_config_path, "a")
-        custom_config.write(f"inventory = {inventory_path}")
-        custom_config.close()
-
         # prompt user for sudo password then run playbook
         ans_run = ansible_runner.run(playbook=docker_playbook_path,
                                      extravars={"user":getpass.getuser(),

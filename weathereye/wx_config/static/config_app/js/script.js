ab78e04b923f626cd1de9812ae34201422630baf
Jeremiah Hammond <jeremiahhammond14@gmail.com>
2024-06-27 22:33:03 -0600

    update

--- weathereye/wx_config/static/config_app/js/script.js
+++ weathereye/wx_config/static/config_app/js/script.js
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Get all elements with the 'unavailable' class
+const unavailableOptions = document.querySelectorAll('.option.unavailable');
+
+// Add event listeners for each element to change text on hover and revert on mouseout
+unavailableOptions.forEach(option => {
+    option.addEventListener('mouseover', function() {
+        this.textContent = 'UNAVAILABLE';
+    });
+
+    option.addEventListener('mouseout', function() {
+        this.textContent = this.dataset.originalText || 'UNAVAILABLE';
+    });
+
+    // Store original text in dataset to revert back on mouseout
+    option.dataset.originalText = option.textContent;
+});

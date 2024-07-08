cace8caddd96207f22c5f8898ad58435fead121a
Jeremiah Hammond <jeremiahhammond14@gmail.com>
2024-07-08 15:14:55 -0600

    collecting static files

--- weathereye/wx_config/config_app/management/commands/resetdb.py
+++ weathereye/wx_config/config_app/management/commands/resetdb.py
@@ -41,35 +41,6 @@ class Command(BaseCommand):
         os.system('python3 manage.py makemigrations')
         self.stdout.write('Applying migrations...')
         os.system('python3 manage.py migrate')
-
-
-        # # start redis
-        # self.stdout.write('Start redis at port 52377...\n')
-        # redis_process = subprocess.Popen(['redis-server', '--port', '52377'])
-
-        # # start django
-        # self.stdout.write('Start django...\n')
-        # django_process = subprocess.Popen(['python3', 'manage.py', 'runserver', 'localhost:52376'])
-
-        # # start celery
-        # self.stdout.write('Start celery worker...\n')
-        # celery_process = subprocess.Popen(['python3', '-m', 'celery', '-A', 'wx_config', 'worker'])
-
-        # # Give some time to let the processes start
-        # time.sleep(5)
-
-        # # Check if all processes are still running
-        # if redis_process.poll() is None:
-        #     self.stdout.write('Redis is running.\n')
-        # else:
-        #     self.stdout.write('Redis failed to start.\n')
-
-        # if django_process.poll() is None:
-        #     self.stdout.write('Django is running.\n')
-        # else:
-        #     self.stdout.write('Django failed to start.\n')
-
-        # if celery_process.poll() is None:
-        #     self.stdout.write('Celery worker is running.\n')
-        # else:
-        #     self.stdout.write('Celery worker failed to start.\n')
+        # Collect static files
+        self.stdout.write('Collecting Static files...')
+        os.system('python3 manage.py collectstatic --noinput --clear')
--- weathereye/wx_config/wx_config/settings.py
+++ weathereye/wx_config/wx_config/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = STATIC_DIR
+
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
